3c1d205add1959c666a3e8282c65d8bc86881cbd
mjkou <1844646307@qq.com>
2018-07-29 13:35:57 +0800

    add format code

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,36 +1,48 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import {addTodo, toggleTodo} from '../actions'
+import React from 'react';
+import { connect } from 'react-redux';
+import { addTodo, toggleTodo } from '../actions';
 
-class AddTodo extends React.PureComponent{
-
-    render(){
-        let input;
-        {
-            const a=1+2;
-        }
-
-        return(
-            <div className="dialog">
-
-                    <input type="text" className="form-control" placeholder="想说点什么" aria-label="Username"
-                       aria-describedby="basic-addon1" ref={node => input = node} />
-
-                    <button type="button" className="btn btn-primary" onClick={() => {
-                        if (!input.value.trim()) {
-                            return
-                        }
-                        this.props.add(input.value);
-                        input.value = ''
-                    }}>Save Task</button>
-
-            </div>
-        )
+class AddTodo extends React.PureComponent {
+  render() {
+    let input;
+    {
+      const b = 1 + 2;
     }
+
+    return (
+      <div className="dialog">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="想说点什么"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+          ref={node => (input = node)}
+        />
+
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => {
+            if (!input.value.trim()) {
+              return;
+            }
+            this.props.add(input.value);
+            input.value = '';
+          }}
+        >
+          Save Task
+        </button>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
-    add: value => dispatch(addTodo(value))
-})
+  add: value => dispatch(addTodo(value))
+});
 
-export default connect(null,mapDispatchToProps)(AddTodo)
+export default connect(
+  null,
+  mapDispatchToProps
+)(AddTodo);
